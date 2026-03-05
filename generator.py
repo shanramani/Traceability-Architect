6c8a00748e8a6ef899179a7ed75597b848973c9c
shanramani <shanramani@outlook.com>
2026-03-05 12:32:25 -0800

    test

--- generator.py
+++ generator.py
@@ -121,6 +121,7 @@ def show_app():
             st.session_state.selected_model = engine_name; st.rerun()
         
         st.markdown('<div class="system-spacer"></div>', unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
         st.markdown('<p class="sb-sub">📂 Target System Context</p>', unsafe_allow_html=True)
         st.file_uploader("SysContext", type="pdf", key="sidebar_sys_uploader", label_visibility="collapsed")
         
