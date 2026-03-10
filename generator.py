67afc01119a75e705e47025f18bbcaa4e9aead74
shanramani <shanramani@outlook.com>
2026-03-10 04:23:34 +0000

    test

--- generator.py
+++ generator.py
@@ -697,7 +697,7 @@ def show_app():
 
         st.markdown('<div class="system-spacer"></div>', unsafe_allow_html=True)
         st.markdown('<div class="sys-context-spacer"></div>', unsafe_allow_html=True)
-        <br>
+        st.markdown("<br>", unsafe_allow_html=True)
         st.markdown('<p class="sb-sub">📂 Target System Context</p>', unsafe_allow_html=True)
         st.file_uploader("SysContext", type="pdf", key="sidebar_sys_uploader", label_visibility="collapsed")
         st.divider()
